66f3c7cd450023c8598a22bbe6a828c94df58ead
subham <subhamsarangi23@gmail.com>
2025-07-15 01:45:30 +0530

    Set up AppContextProvider and import Bootstrap for styling

--- invoice-generator-client/src/main.jsx
+++ invoice-generator-client/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+import {AppContextProvider} from './context/AppContext.jsx'
 import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
@@ -6,7 +6,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <AppContextProvider>
     <App />
-  </StrictMode>,
+  </AppContextProvider>,
 )
