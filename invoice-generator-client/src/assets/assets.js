3dd13738ecbe7141481f63c5c4a925556d34efbe
subham <subhamsarangi23@gmail.com>
2025-07-15 01:13:59 +0530

    create a object of image

--- invoice-generator-client/src/assets/assets.js
+++ invoice-generator-client/src/assets/assets.js
@@ -1,3 +1,13 @@
-export const assets = {
-    
+import one from './one.png'
+import two from './two.png'
+import three from './three.png'
+import four from './four.png'
+import upload from "./upload.png"
+
+export  const assets = {
+    one,
+    two,
+    three,
+    four,
+    upload
 }
