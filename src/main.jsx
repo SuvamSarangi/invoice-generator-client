e261c13fa8ded41658f99a0b38e32f08f21c3284
subham <subhamsarangi23@gmail.com>
2025-11-15 16:59:47 +0530

    wrape with clerkProvider component

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,20 @@
-import {AppContextProvider} from './context/AppContext.jsx'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import './index.css'
-import App from './App.jsx'
+import { AppContextProvider } from "./context/AppContext.jsx";
+import { createRoot } from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.bundle.js";
+import "./index.css";
+import App from "./App.jsx";
+import {ClerkProvider} from '@clerk/clerk-react';
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
-createRoot(document.getElementById('root')).render(
+if (!PUBLISHABLE_KEY) {
+  throw new Error("PUBLISHABLE_KEY is missing !!");
+}
+
+createRoot(document.getElementById("root")).render(
   <AppContextProvider>
-    <App />
-  </AppContextProvider>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </AppContextProvider>
+);
